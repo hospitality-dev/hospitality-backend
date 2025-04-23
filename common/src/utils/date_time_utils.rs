4da90f6142af9de9316920c261aec2ca4afbfe34
Mihailo <mihailo.dilparic@gmail.com>
2025-04-23 12:53:15 +0200

    feat: format date time util

--- common/src/utils/date_time_utils.rs
+++ common/src/utils/date_time_utils.rs
@@ -4,3 +4,7 @@ use chrono_tz::Tz;
 pub fn convert_to_tz(utc: DateTime<Utc>, tz: Tz) -> DateTime<Tz> {
     utc.with_timezone(&tz)
 }
+
+pub fn format_to_string(date: DateTime<Utc>, format: String) -> String {
+    date.format(&format).to_string()
+}
