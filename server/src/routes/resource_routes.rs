cf4aecfd1dd4121b8133f57ea60d257898c8dc8c
Mihailo <mihailo.dilparic@gmail.com>
2025-04-25 08:11:16 +0200

    fix: allowed fields for countries route

--- server/src/routes/resource_routes.rs
+++ server/src/routes/resource_routes.rs
@@ -1,25 +1,42 @@
-use axum::{extract::State, routing::get, Extension, Router};
-use serde_json::Value;
+use std::collections::HashSet;
 
 use crate::{
     enums::errors::AppError,
-    middlware::crud_middleware::AllowedFieldsType,
     models::{
+        requests::QueryParams,
         response::{AppResponse, RouteResponse},
         state::AppState,
     },
     traits::db_traits::SerializeList,
 };
+use axum::{
+    extract::{Query, State},
+    routing::get,
+    Router,
+};
+use common::consts::COUNTRIES_FIELDS;
+use itertools::Itertools;
+use serde_json::Value;
 
 async fn countries(
     State(state): State<AppState>,
-    Extension(fields): Extension<AllowedFieldsType>,
+    query: Query<QueryParams>,
 ) -> RouteResponse<Value> {
+    let query_fields = query.0.fields.unwrap_or("".into());
+    let fields: HashSet<&str> = query_fields.split(",").collect::<HashSet<&str>>();
+
+    let allowed_fields: HashSet<&str> = fields
+        .intersection(&HashSet::from_iter(COUNTRIES_FIELDS))
+        .map(|item| *item)
+        .collect();
+
+    let allowed_fields = allowed_fields.iter().join(", ");
+
     let conn = state.get_db_conn().await?;
 
     let rows = conn
         .query(
-            &format!("SELECT {} FROM countries ORDER BY title;", fields),
+            &format!("SELECT {} FROM countries ORDER BY title;", allowed_fields),
             &[],
         )
         .await
