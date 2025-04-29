59f31461a3789847d9d8971604a594f7220d929e
Mihailo <mihailo.dilparic@gmail.com>
2025-04-29 12:57:15 +0200

    feat: supplier contacts

--- server/src/routes/contacts_routes.rs
+++ server/src/routes/contacts_routes.rs
@@ -163,12 +163,64 @@ async fn list_contacts_store(
     }
 }
 
+async fn list_contacts_suppliers(
+    Extension(session): Extension<AuthSession>,
+    Extension(fields): Extension<AllowedFieldsType>,
+    State(state): State<AppState>,
+    Path(supplier_id): Path<Uuid>,
+) -> RouteResponse<Value> {
+    let mut attr = HashMap::new();
+    attr.insert(
+        "location_id".to_string(),
+        CerbosAttrValue::Uuid(session.user.location_id.unwrap()),
+    );
+    attr.insert(
+        "company_id".to_string(),
+        CerbosAttrValue::Uuid(session.user.company_id.unwrap()),
+    );
+    let principal = session.user.to_principal();
+
+    let resource = CerbosResource {
+        id: supplier_id.to_string(),
+        kind: Models::Suppliers.to_string(),
+        attr: Some(attr),
+        scope: None,
+    };
+    let check = CerbosCheck {
+        principal,
+        resources: vec![resource],
+        action: Actions::View(supplier_id),
+        model: Models::Suppliers,
+    };
+
+    let has_permission = check.check_permission(&state).await?;
+    if has_permission {
+        let mapped_fields = fields.replace("contacts.", "suppliers_contacts.");
+        let conn = &state.get_db_conn().await?;
+        let result = conn
+            .query(
+                &format!(
+                    "SELECT {} FROM suppliers_contacts WHERE parent_id = $1;",
+                    mapped_fields
+                ),
+                &[&supplier_id],
+            )
+            .await
+            .map_err(AppError::db_error)?;
+
+        return Ok(AppResponse::default_response(result.serialize_list(true)));
+    } else {
+        return Err(AppError::forbidden_response("User is not permitted to perform this action. MODEL suppliers | ACTION view | contacts_list"));
+    }
+}
+
 pub fn contacts_routes() -> Router<AppState> {
     Router::new().nest(
         "/contacts",
         Router::new()
             .route("/list/location/{location_id}", get(list_contacts_location))
             .route("/list/user/{user_id}", get(list_contacts_user))
-            .route("/list/store/{store_id}", get(list_contacts_store)),
+            .route("/list/store/{store_id}", get(list_contacts_store))
+            .route("/list/supplier/{supplier_id}", get(list_contacts_suppliers)),
     )
 }
