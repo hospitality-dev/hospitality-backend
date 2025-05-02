7049cd630f2f2fb5fe07759f1b0f5e3cae455ca5
Mihailo <mihailo.dilparic@gmail.com>
2025-05-02 18:21:54 +0200

    feat: spending per store last 30 days

--- server/src/routes/statistics_routes.rs
+++ server/src/routes/statistics_routes.rs
@@ -11,7 +11,7 @@ use crate::{
     traits::db_traits::SerializeList,
 };
 
-async fn last_month_purchase_per_store(
+async fn last_month_purchase_per_date(
     Extension(session): Extension<AuthSession>,
     State(state): State<AppState>,
 ) -> RouteResponse<Value> {
@@ -38,12 +38,49 @@ async fn last_month_purchase_per_store(
     return Ok(AppResponse::default_response(rows.serialize_list(true)));
 }
 
+async fn last_month_purchase_per_suppiler(
+    Extension(session): Extension<AuthSession>,
+    State(state): State<AppState>,
+) -> RouteResponse<Value> {
+    let stmt = "SELECT suppliers.title, SUM(total) as total
+    FROM
+        purchases
+    INNER JOIN stores
+        ON purchases.store_id = stores.id
+    INNER JOIN suppliers
+        ON stores.parent_id = suppliers.id
+    WHERE
+        purchases.location_id = $1
+            AND
+        purchased_at IS NOT NULL
+            AND
+        purchased_at >= NOW() - INTERVAL '30 days'
+    GROUP BY
+        suppliers.title, suppliers.id
+    ORDER BY
+        total;";
+
+    let conn = &state.get_db_conn().await?;
+
+    let rows = conn
+        .query(stmt, &[&session.user.location_id.unwrap()])
+        .await
+        .map_err(AppError::db_error)?;
+
+    return Ok(AppResponse::default_response(rows.serialize_list(true)));
+}
+
 pub fn statistics_routes() -> Router<AppState> {
     return Router::new().nest(
         "/statistics",
-        Router::new().route(
-            "/purchases/month",
-            axum::routing::get(last_month_purchase_per_store),
-        ),
+        Router::new()
+            .route(
+                "/purchases/month",
+                axum::routing::get(last_month_purchase_per_date),
+            )
+            .route(
+                "/purchases/month/suppliers",
+                axum::routing::get(last_month_purchase_per_suppiler),
+            ),
     );
 }
